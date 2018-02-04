fdd2297d6fd84f9c2cd18755c269e02fc268a30b
Sef <null>
2018-02-04 02:40:53 -0500

    Added tests for MsgVerAck

---
+++ NDecred.Wire.Tests/MsgVerAckTests.cs
@@ -0,0 +1,19 @@
+﻿using NDecred.Wire;
+using Xunit;
+
+namespace NDecred.Network.Tests
+{
+    public class MsgVerAckTests
+    {
+        [Fact]
+        public void DecodeEncode_GivenNoDataToDeserialize_ReturnsNoDataWhenSerialized()
+        {
+            var subject = new MsgVerAck();
+            subject.Decode(new byte[0]);
+            var result = subject.Encode();
+            
+            Assert.True(result != null);
+            Assert.Equal(0, result.Length);
+        }
+    }
+}
